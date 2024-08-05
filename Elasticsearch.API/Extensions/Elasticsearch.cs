73820a58bb567a977edcf4c4e1494ef1bb49b508
murcell <murselgurkan@gmail.com>
2024-08-05 16:03:35 +0300

    elastic DI'ya eklendi

---
+++ Elasticsearch.API/Extensions/Elasticsearch.cs
@@ -0,0 +1,18 @@
+﻿using Elasticsearch.Net;
+using Nest;
+
+namespace Elasticsearch.API.Extensions
+{
+    public static class Elasticsearch
+    {
+        public static void AddElastic(this IServiceCollection services, IConfiguration configuration) 
+        {
+            var pool = new SingleNodeConnectionPool(new Uri(configuration.GetSection("Elastic")["Url"]!));
+            var setttings = new ConnectionSettings(pool);
+            //setttings.BasicAuthentication(); usr dn pass will write here
+            var client = new ElasticClient(setttings);
+            services.AddSingleton(client);
+          
+        }
+    }
+}
