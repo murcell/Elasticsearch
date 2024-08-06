611af772a35431ece825c75224cdeef33470af46
murcell <murselgurkan@gmail.com>
2024-08-06 15:37:25 +0300

    Query Term

---
+++ Elasticsearch.API/Controllers/ECommerceController.cs
@@ -0,0 +1,26 @@
+﻿using Elasticsearch.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elasticsearch.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ECommerceController : ControllerBase
+    {
+        // normalde servis clası oluşturup yapmak lazım.
+        // bir öndeki kontrollerde yaptığım için burada direk repo üzerinden çağırdım
+        private readonly ECommerceRepository _eCommerceRepository;
+
+        public ECommerceController(ECommerceRepository eCommerceRepository)
+        {
+            _eCommerceRepository = eCommerceRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> TermQuery(string customerFirstName) 
+        {
+            return Ok(await _eCommerceRepository.TermQuery(customerFirstName));
+        }
+    }
+}
