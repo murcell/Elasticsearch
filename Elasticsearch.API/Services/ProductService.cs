cda13dd936a96fb67a961ab6ff7ab1c8de9a55c2
murcell <murselgurkan@gmail.com>
2024-08-06 11:42:18 +0300

    product servis yeni ktüphane için hazırlandı

--- Elasticsearch.API/Services/ProductService.cs
+++ Elasticsearch.API/Services/ProductService.cs
@@ -1,6 +1,6 @@
 ﻿using Elasticsearch.API.DTOs;
 using Elasticsearch.API.Repositories;
-using Nest;
+//using Nest;
 using System.Net;
 
 namespace Elasticsearch.API.Services
@@ -79,45 +79,137 @@ namespace Elasticsearch.API.Services
         {
             var deleteResponse = await _productRepository.DeleteAsync(id);
 
-            if (!deleteResponse.IsValid && deleteResponse.Result == Result.NotFound)
+            if (!deleteResponse.IsValidResponse && deleteResponse.Result == Result.NotFound)
             {
                 return ResponseDto<bool>.Fail(new List<string> { "Silmeye çalıştığınız ürün bulunamamıştır." }, System.Net.HttpStatusCode.NotFound);
 
             }
 
-            if (!deleteResponse.IsValid)
+            if (!deleteResponse.IsValidResponse)
             {
-                _logger.LogError(deleteResponse.OriginalException, deleteResponse.ServerError.Error.ToString());
+                deleteResponse.TryGetOriginalException(out Exception? exception);
 
-                return ResponseDto<bool>.Fail(new List<string> { "Silme esnasında bir hata meydana geldi." }, System.Net.HttpStatusCode.InternalServerError);
+                _logger.LogError(exception, deleteResponse.ElasticsearchServerError.Error.ToString());
+
+                return ResponseDto<bool>.Fail(new List<string> { "silme esnasında bir hata meydana geldi." }, System.Net.HttpStatusCode.InternalServerError);
+
             }
 
             return ResponseDto<bool>.Success(true, HttpStatusCode.NoContent);
         }
 
+        #region NEST Kütüphanesi ile CRUD
+        //private readonly ProductRepository _productRepository;
+        //private readonly ILogger<ProductService> _logger;
+        //public ProductService(ProductRepository productRepository, ILogger<ProductService> logger)
+        //{
+        //    _productRepository = productRepository;
+        //    _logger = logger;
+        //}
+
+        //public async Task<ResponseDto<ProductDto>> SaveAsync(ProductCreateDto request)
+        //{
+        //    var responseProduct = await _productRepository.SaveAsync(request.CreateProduct());
+        //    if (responseProduct == null)
+        //    {
+        //        return ResponseDto<ProductDto>.Fail(new List<string> { "Kayıt esnasında bir hata meydana geldi." }, HttpStatusCode.InternalServerError);
+        //    }
+
+        //    return ResponseDto<ProductDto>.Success(responseProduct.CreateDto(), HttpStatusCode.Created);
+        //}
+
+        //public async Task<ResponseDto<List<ProductDto>>> GetAllAsync()
+        //{
+        //    var products = await _productRepository.GetAllAsync();
+        //    var productListDto = new List<ProductDto>();
+
+        //    foreach (var x in products)
+        //    {
+        //        if (x.Feature is null)
+        //        {
+        //            productListDto.Add(new ProductDto(x.Id, x.Name, x.Price, x.Stock, null));
+
+        //            continue;
+        //        }
+
+        //        productListDto.Add(new ProductDto(x.Id, x.Name, x.Price, x.Stock, new ProductFeatureDto(x.Feature.Width, x.Feature!.Height, x.Feature!.Color.ToString())));
+
+        //    }
+
+        //    return ResponseDto<List<ProductDto>>.Success(productListDto, HttpStatusCode.OK);
+
+        //}
+
+        //public async Task<ResponseDto<ProductDto>> GetByIdAsync(string id)
+        //{
+        //    var hasProduct = await _productRepository.GetByIdAsync(id);
+
+        //    if (hasProduct == null)
+        //    {
+        //        return ResponseDto<ProductDto>.Fail("ürün bulunamadı", HttpStatusCode.NotFound);
+        //    }
+
+        //    var productDto = hasProduct.CreateDto();
+
+        //    return ResponseDto<ProductDto>.Success(productDto, HttpStatusCode.OK);
+        //}
+
+        //public async Task<ResponseDto<bool>> UpdateAsync(ProductUpdateDto updateProduct)
+        //{
+        //    var isSuccess = await _productRepository.UpdateSynch(updateProduct);
+
+        //    if (!isSuccess)
+        //    {
+        //        return ResponseDto<bool>.Fail(new List<string> { "update esnasında bir hata meydana geldi." }, System.Net.HttpStatusCode.InternalServerError);
+        //    }
+
+        //    return ResponseDto<bool>.Success(true, HttpStatusCode.NoContent);
+        //}
 
         //public async Task<ResponseDto<bool>> DeleteAsync(string id)
         //{
         //    var deleteResponse = await _productRepository.DeleteAsync(id);
 
-        //    if (!deleteResponse.IsValidResponse && deleteResponse.Result == Result.NotFound)
+        //    if (!deleteResponse.IsValid && deleteResponse.Result == Result.NotFound)
         //    {
         //        return ResponseDto<bool>.Fail(new List<string> { "Silmeye çalıştığınız ürün bulunamamıştır." }, System.Net.HttpStatusCode.NotFound);
 
         //    }
 
-        //    if (!deleteResponse.IsValidResponse)
+        //    if (!deleteResponse.IsValid)
         //    {
-        //        deleteResponse.TryGetOriginalException(out Exception? exception);
+        //        _logger.LogError(deleteResponse.OriginalException, deleteResponse.ServerError.Error.ToString());
 
-        //        _logger.LogError(exception, deleteResponse.ElasticsearchServerError.Error.ToString());
-
-        //        return ResponseDto<bool>.Fail(new List<string> { "silme esnasında bir hata meydana geldi." }, System.Net.HttpStatusCode.InternalServerError);
-
+        //        return ResponseDto<bool>.Fail(new List<string> { "Silme esnasında bir hata meydana geldi." }, System.Net.HttpStatusCode.InternalServerError);
         //    }
 
         //    return ResponseDto<bool>.Success(true, HttpStatusCode.NoContent);
         //}
+
+
+        ////public async Task<ResponseDto<bool>> DeleteAsync(string id)
+        ////{
+        ////    var deleteResponse = await _productRepository.DeleteAsync(id);
+
+        ////    if (!deleteResponse.IsValidResponse && deleteResponse.Result == Result.NotFound)
+        ////    {
+        ////        return ResponseDto<bool>.Fail(new List<string> { "Silmeye çalıştığınız ürün bulunamamıştır." }, System.Net.HttpStatusCode.NotFound);
+
+        ////    }
+
+        ////    if (!deleteResponse.IsValidResponse)
+        ////    {
+        ////        deleteResponse.TryGetOriginalException(out Exception? exception);
+
+        ////        _logger.LogError(exception, deleteResponse.ElasticsearchServerError.Error.ToString());
+
+        ////        return ResponseDto<bool>.Fail(new List<string> { "silme esnasında bir hata meydana geldi." }, System.Net.HttpStatusCode.InternalServerError);
+
+        ////    }
+
+        ////    return ResponseDto<bool>.Success(true, HttpStatusCode.NoContent);
+        ////} 
+        #endregion
 
     }
 }
