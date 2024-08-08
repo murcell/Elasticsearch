7852ec6c7535441c39bb497630821a5e8dd5b2d2
murcell <murselgurkan@gmail.com>
2024-08-08 11:53:37 +0300

    ECommerce search

---
+++ Elastiicsearch.Web/Controllers/ECommerceController.cs
@@ -0,0 +1,30 @@
+﻿using Elastiicsearch.Web.Services;
+using Elastiicsearch.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elastiicsearch.Web.Controllers
+{
+	public class ECommerceController : Controller
+	{
+		private readonly ECommerceService _service;
+
+		public ECommerceController(ECommerceService service)
+		{
+			_service = service;
+		}
+
+		public async Task<IActionResult> Search([FromQuery] SearchPageViewModel searchPageView)
+		{
+			var (eCommerceList, totalCount, pageLinkCount) = await _service.SearchAsync(searchPageView.SearchViewModel, searchPageView.Page,
+				searchPageView.PageSize);
+
+
+			searchPageView.List = eCommerceList;
+			searchPageView.TotalCount = totalCount;
+			searchPageView.PageLinkCount = pageLinkCount;
+
+
+			return View(searchPageView);
+		}
+	}
+}
